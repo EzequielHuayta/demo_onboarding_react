fa021703445cf39a7de83b3e19534e29233e4317
Ezequiel Huayta Romay <ezequiellautaro.huaytaromay.contractor@gdevtools.com>
2024-11-18 00:30:07 -0300

    Se agrega imagenes y login funcionando + css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './components/Login';
 import Home from './components/Home';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import '../css/Login.css';  
+import santanderLogo from '../assets/santanderLogo.png';
 
 const Login = () => {
   const [legajo, setLegajo] = useState('');
@@ -15,7 +17,7 @@ const handleLogin = async () => {
         {},
         {
           headers: {
-            'legajo': legajo,
+            'legajo': legajo.toUpperCase(),
             'password': password,
           },
         }
@@ -49,41 +51,55 @@ const handleLogin = async () => {
   
 
   return (
-    <div className="container mt-5">
-      <div className="row justify-content-center">
-        <div className="col-md-4">
-          <h3>Bienvenido al onboarding del Santander</h3>
-          {error && <div className="alert alert-danger">{error}</div>}
-          <form
-            onSubmit={(e) => {
-              e.preventDefault();
-              handleLogin();
-            }}
-          >
-            <div className="mb-3">
-              <label className="form-label">legajo</label>
-              <input
-                type="text"
-                className="form-control"
-                value={legajo}
-                onChange={(e) => setLegajo(e.target.value)}
-              />
-            </div>
-            <div className="mb-3">
-              <label className="form-label">Password</label>
-              <input
-                type="password"
-                className="form-control"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </div>
-            <button type="submit" className="btn btn-primary">
-              Entrar
-            </button>
-          </form>
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid" style={{justifyContent: 'center'}}>
+          <a className="navbar-brand" href="/">
+            <img src={santanderLogo} alt="Santander" style={{ height: '40px' }} />
+          </a>
+
+        </div>
+      </nav>
+
+      <div className="container mt-5">
+        <div className="row justify-content-center">
+          <div className="col-md-9">
+            <h3 className="text-center mb-4">Bienvenido al onboarding del Santander</h3>
+            <form
+              onSubmit={(e) => {
+                e.preventDefault();
+                handleLogin();
+              }}
+            >
+              <div className="form-group">
+                <label className="form-label">Legajo</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  value={legajo}
+                  onChange={(e) => setLegajo(e.target.value)}
+                />
+              </div>
+              <div className="mb-3">
+                <label className="form-label">Contraseña</label>
+                <input
+                  type="password"
+                  className="form-control"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+              <button type="submit" className="btn btn-primary w-100">
+                Entrar
+              </button>
+
+              {error && <div className="alert alert-danger">{error}</div>}
+              
+            </form>
+          </div>
         </div>
       </div>
+      
     </div>
   );
 };
